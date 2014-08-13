880260440113002a6a8c72250f884c944069af29
kevinychen <binonunquineist@gmail.com>
2014-08-12 18:10:59 -0700

    Basic framework for pyrostore.

--- client.js
+++ client.js
@@ -1,2 +1,10 @@
-console.log('hi');
+var postgres = require('./postgres');
+
+var user = process.argv[2];
+var pw = process.argv[3];
+var url = process.argv[4];
+var table = process.argv[5];
+var db = new postgres.Client({user: user, pw: pw, url: url, table: table});
+
+db.insert(['root', 'child'], 'ho', console.log);
 
--- postgres.js
+++ postgres.js
@@ -1,30 +1,116 @@
 /*
  * Client for postgres.
  *
- * run "npm install pg"
+ * Client setup:
+ *   run "npm install pg"
+ *
+ * Postgres setup:
+ *   create table planes (key text unique, value text);
  */
 
 var pg = require('pg');
 
-function Postgres(user, pw, url, db) {
-    this.user = user || 'postgres';
-    this.pw = pw || '';
-    this.url = url || 'localhost';
-    this.db = db || 'pyrostore';
+function Postgres(params) {
+    this.user = params.user || 'postgres';
+    this.pw = params.pw || '';
+    this.url = params.url || 'localhost';
+    this.db = params.db || 'pyrostore';
+    this.table = params.table || 'table';
     this.connection = 'postgres://' + this.user + ':' + this.pw + '@' + this.url + '/' + this.db;
 }
 
-Postgres.prototype.sampleQuery = function(callback) {
+Postgres.prototype.query = function(query, args, callback) {
     pg.connect(this.connection, function(err, client, done) {
         if (err) {
             callback(err);
         } else {
-            client.query('select $1::int as number', ['1'], function(err, result) {
+            console.log(query);
+            console.log(args);
+            client.query(query, args, function(err, result) {
                 done();
-                callback(err, result);
+                callback(err, result && result.rows);
             });
         }
     });
+};
+
+/*
+ * retrieve('/root/child', callback)
+ *   -> callback(err, {grandchild: {leaf: 1, another_leaf: 2}}, '/root/child')
+ */
+Postgres.prototype.retrieve = function(path, callback) {
+    this.query('select value from ' + this.table + ' where key = $1', [path], function(err, rows) {
+        if (err) {
+            callback(err);
+        } else if (rows.length === 0) {
+            callback(false, {}, path);
+        } else {
+            callback(err, JSON.parse(rows[0].value), path);
+        }
+    });
+}
+
+/*
+ * get(['root', 'child'], callback)
+ *   -> callback(err, {grandchild: {leaf: 1, another_leaf: 2}})
+ */
+Postgres.prototype.get = function(key, callback) {
+    var path = '/' + key.join('/');
+    this.retrieve(path, callback);
+}
+
+/*
+ * load(['/root/child/grandchild/leaf', 'value', callback)
+ *   -> callback(err)
+ */
+Postgres.prototype.load = function(path, obj, callback) {
+    var value = JSON.stringify(obj);
+    var me = this;
+    // Upsert
+    this.query('insert into ' + this.table + ' (key, value) values ($1, $2)',
+            [path, value], function(err) {
+         if (err) {
+             me.query('update ' + me.table + ' set value = $2 where key = $1',
+                     [path, value], callback);
+         } else {
+             callback();
+         }
+    });
+}
+
+/*
+ * insert(['root', 'child', 'grandchild', 'leaf'], 'value', callback)
+ */
+Postgres.prototype.insert = function(key, value, callback) {
+    var path = '';
+    var counter = key.length;
+    var me = this;
+    var end = function(err) {
+        if (err || --counter === 0) {
+            callback(err);
+        }
+    };
+    for (var i = 0; i + 1 < key.length; i++) {
+        path += '/' + key[i];
+        this.retrieve(path, function(err, baseObj, path) {
+            if (err) {
+                callback(err);
+            } else {
+                var obj = baseObj;
+                for (var j = i; j + 1 < key.length; j++) {
+                    var k = key[i];
+                    if (!(k in obj)) {
+                        obj[k] = {};
+                    }
+                    obj = obj[k];
+                }
+                obj[key[key.length - 1]] = value;
+                me.load(path, baseObj, end);
+            }
+        });
+    }
+    path += '/' + key[key.length - 1];
+    this.load(path, value, end);
 };
 
 exports.Client = Postgres;
