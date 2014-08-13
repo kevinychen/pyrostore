3ff1e99d428ae6e3f4d39b3bbe531e53e7973db1
kevinychen <binonunquineist@gmail.com>
2014-08-12 23:37:51 -0700

    Retrieve data by combining leaves.

--- client.js
+++ client.js
@@ -1,10 +1,14 @@
 var postgres = require('./postgres');
 
 var user = process.argv[2];
-var pw = process.argv[3];
-var url = process.argv[4];
+var password = process.argv[3];
+var host = process.argv[4];
 var table = process.argv[5];
-var db = new postgres.Client({user: user, pw: pw, url: url, table: table});
-
-db.insert(['root', 'child'], 'ho', console.log);
+var db = new postgres.Client({
+    user: user,
+    password: password,
+    host: host,
+    table: table
+});
+db.get('root', console.log);
 
--- postgres.js
+++ postgres.js
@@ -8,109 +8,73 @@
  *   create table planes (key text unique, value text);
  */
 
-var pg = require('pg');
+var Client = require('pg').Client;
 
 function Postgres(params) {
-    this.user = params.user || 'postgres';
-    this.pw = params.pw || '';
-    this.url = params.url || 'localhost';
-    this.db = params.db || 'pyrostore';
     this.table = params.table || 'table';
-    this.connection = 'postgres://' + this.user + ':' + this.pw + '@' + this.url + '/' + this.db;
+    this.client = new Client({
+        user: params.user || 'postgres',
+        password: params.password || '',
+        database: params.database || 'pyrostore',
+        host: params.host || 'localhost',
+        port: params.port || 5432
+    });
+    this.client.connect();
 }
 
+/*
+ * query('select name from table where id = $1', [1], callback)
+ *   -> callback(err, [{name: 'kyc'}, {name: 'becky'}])
+ */
 Postgres.prototype.query = function(query, args, callback) {
-    pg.connect(this.connection, function(err, client, done) {
-        if (err) {
-            callback(err);
-        } else {
-            console.log(query);
-            console.log(args);
-            client.query(query, args, function(err, result) {
-                done();
-                callback(err, result && result.rows);
-            });
-        }
+    this.client.query(query, args, function(err, result) {
+        callback(err, result && result.rows);
     });
 };
 
 /*
- * retrieve('/root/child', callback)
- *   -> callback(err, {grandchild: {leaf: 1, another_leaf: 2}}, '/root/child')
+ * addAttr({'a': 1}, ['b', 'c'], 2)
+ *   -> {'a': 1, 'b': {'c': 2}}
  */
-Postgres.prototype.retrieve = function(path, callback) {
-    this.query('select value from ' + this.table + ' where key = $1', [path], function(err, rows) {
-        if (err) {
-            callback(err);
-        } else if (rows.length === 0) {
-            callback(false, {}, path);
-        } else {
-            callback(err, JSON.parse(rows[0].value), path);
+function addAttr(obj, attrs, value) {
+    var sentinel = {obj: obj};
+    var exAttrs = ['obj'].concat(attrs);
+    var ptr = sentinel;
+    for (var i = 0; i < attrs.length; i++) {
+        if (!(exAttrs[i] in ptr) || typeof(ptr[exAttrs[i]]) !== 'object') {
+            ptr[exAttrs[i]] = {};
         }
-    });
+        ptr = ptr[exAttrs[i]];
+    }
+    ptr[exAttrs[attrs.length]] = value;
+    return sentinel.obj;
 }
 
 /*
- * get(['root', 'child'], callback)
+ * get('root/child', callback)
  *   -> callback(err, {grandchild: {leaf: 1, another_leaf: 2}})
  */
-Postgres.prototype.get = function(key, callback) {
-    var path = '/' + key.join('/');
-    this.retrieve(path, callback);
-}
-
-/*
- * load(['/root/child/grandchild/leaf', 'value', callback)
- *   -> callback(err)
- */
-Postgres.prototype.load = function(path, obj, callback) {
-    var value = JSON.stringify(obj);
-    var me = this;
-    // Upsert
-    this.query('insert into ' + this.table + ' (key, value) values ($1, $2)',
-            [path, value], function(err) {
-         if (err) {
-             me.query('update ' + me.table + ' set value = $2 where key = $1',
-                     [path, value], callback);
-         } else {
-             callback();
-         }
-    });
-}
-
-/*
- * insert(['root', 'child', 'grandchild', 'leaf'], 'value', callback)
- */
-Postgres.prototype.insert = function(key, value, callback) {
-    var path = '';
-    var counter = key.length;
-    var me = this;
-    var end = function(err) {
-        if (err || --counter === 0) {
-            callback(err);
-        }
-    };
-    for (var i = 0; i + 1 < key.length; i++) {
-        path += '/' + key[i];
-        this.retrieve(path, function(err, baseObj, path) {
-            if (err) {
-                callback(err);
-            } else {
-                var obj = baseObj;
-                for (var j = i; j + 1 < key.length; j++) {
-                    var k = key[i];
-                    if (!(k in obj)) {
-                        obj[k] = {};
-                    }
-                    obj = obj[k];
-                }
-                obj[key[key.length - 1]] = value;
-                me.load(path, baseObj, end);
-            }
-        });
+Postgres.prototype.get = function(path, callback) {
+    if (path.slice(-1) !== '/') {
+        path += '/';
     }
-    path += '/' + key[key.length - 1];
-    this.load(path, value, end);
+    this.query("select key, value from " + this.table +
+            " where key LIKE '" + path + "%'", [], function(err, results) {
+        if (err) {
+            callback(err);
+        } else {
+            var obj = undefined;
+            for (var i = 0; i < results.length; i++) {
+                var result = results[i];
+                var relPath = result.key.substr(path.length);
+                var attrs = relPath.split('/').filter(function(attr) {
+                    return attr;
+                });
+                obj = addAttr(obj, attrs, result.value);
+            }
+            callback(false, obj);
+        }
+    });
 };
 
 exports.Client = Postgres;
